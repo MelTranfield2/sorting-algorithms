a6b8bb41ec85d846636dd4aba0029de7f9f3cc5a
Melissa Tranfield <mrotranfield@gmail.com>
2020-05-20 20:44:39 +0100

    binarySearch random number generated, csv file

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -1,38 +1,107 @@
 package com.company;
 
+import java.io.FileWriter;
 import java.util.Random;
+import java.util.Scanner;
 
 public class BinarySearch {
 
-    public static void main(String[] args) { new BinarySearchTimer().runTimer();
+    public static void main(String[] args) { new BinarySearchTimer(runBinaryTimer());
     }
-}
 
-class BinarySearchTimer extends SortTimer {
+//    private int[] generateRandomArray(int arraySize) {
+//        Random rand = new Random();
+//
+//        return rand.ints(arraySize, 0, 1000).toArray();
+//    }
 
-    public int[] Sort(int[] arr) {
+    public static int generateRandomInt(int arraySize) {
 
-            int count = 0;
-            int MIN = arr[0];
-            int MAX = arr[50];
-            while (true) {
-                int guess = (MAX + MIN)/2;
-                System.out.println("Is your guess " + guess + "?");
+        //returns initialGuess
 
-                String response = System.console().readLine();
-                if ("correct".equals(response.toLowerCase())){
-                    break;
-                } else if("lower".equals(response.toLowerCase())){
-                    MAX = guess;
-                } else if("higher".equals(response.toLowerCase())){
-                    MIN = guess;
-                }
-                count++;
-                System.out.println(MIN);
-                System.out.println(MAX);
-            }
-            System.out.println("thank you for playing");
+        Random r = new Random();
+        return r.nextInt(arraySize);
+    }
+
+    public static int runBinaryTimer() {
+
+        int initialGuess = 0;
+
+        int array[] = {1000, 2000, 3000, 4000};
+        for (int arraySize : array) {
+            initialGuess = generateRandomInt(arraySize);
+
+            System.out.println(initialGuess);
         }
-        return arr;
+
+    return initialGuess;
+        }
     }
-}
+
+class BinarySearchTimer {
+
+    public String getCsvFileName() {
+        return "binarySearch.csv";
+    }
+
+    public void appendtoCsvFile(int arraySize, int count) {
+
+        try {
+            FileWriter csvWriter = new FileWriter(getCsvFileName(), true);
+            csvWriter.write(arraySize + "," + count + "\n");
+
+            csvWriter.flush();
+            csvWriter.close();
+        } catch (Exception e) {
+            System.err.println("Error: failed to write to file");
+        }
+    }
+
+   public BinarySearchTimer(int initialGuess) {
+
+        System.out.println("xxxx " + initialGuess);
+            int count = 0;
+            int MIN = 0;
+            int MAX = 4000;
+            if (initialGuess < MAX)
+                while (true) {
+                    int guess = (MAX + MIN)/2;
+                    System.out.println("Is your guess " + guess + "?");
+
+                    if (guess == initialGuess) {
+                        System.out.println("your number is " + guess);
+                        break;
+                    } else if (guess < initialGuess) {
+                        System.out.println("guessing lower than " + guess + " ...");
+                        MIN = guess;
+                    } else {
+                        System.out.println("guessing higher than " + guess + " ...");
+                        MAX = guess;
+                    }
+                    count++;
+                    System.out.println(MIN);
+                    System.out.println(MAX);
+                }
+            else {
+                System.out.println("initialGuess is greater than MAX range ");
+            }
+
+            System.out.println("thank you for playing " + count + " times");
+
+            appendtoCsvFile(initialGuess, count);
+        }
+    }
+
+
+
+
+//                String response = s.nextLine();
+//                if ("correct".equals(response.toLowerCase())){
+//                    break;
+//                } else if("lower".equals(response.toLowerCase())){
+//                    MAX = guess;
+//                } else if("higher".equals(response.toLowerCase())){
+//                    MIN = guess;
+//                }
+
+//            Scanner s = new Scanner(System.in);
--- src/com/company/BinaryTimer.java
+++ src/com/company/BinaryTimer.java
@@ -71,4 +71,4 @@ public abstract class BinaryTimer {
         }
 
     }
-}
+
